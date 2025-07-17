bd10ae6e0b86d2df4eb898b7875160ef2925714b
“Shiju <shijujohn1093@gmail.com>
2025-07-18 00:18:19 +0530

    code changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
+import time
+import asyncio
 from app.controllers import example_controller
 
 
@@ -21,3 +23,64 @@ def read_root():
         "environment": environment,
         "loaded_env_file": env_file
     }
+
+
+
+
+"""
+Runs in main thread 
+No awaiteable operations, cannot be paused
+Sequentioal order
+Request 1
+    Hello
+    Bye
+Request 2
+    Hello
+    Bye
+
+"""
+@app.get("/1")
+async def endpoint1(): # Processed sequentially 
+    print("Hello")
+    time.sleep(5) # Blocking I/O operation, cannot be awaited
+    # Function execution cannot be paused
+    print("Bye") 
+
+
+"""
+Runs in main thread 
+Has awaitable operaion, can be paused
+
+Request 1
+    Hello
+Request 2
+    Hello
+Request 1
+    Bye
+Request 2
+    Bye
+"""
+@app.get("/2")
+async def endpoint2():    # Processed Concurrently
+    print("Hello")
+    await asyncio.sleep(5)    # Non-blocking I/O operation, can be awaited
+    # Function execution paused
+    print("Bye")
+
+
+"""
+Runs in separate thread, this is the reason all the processing done parallelly
+"""
+@app.get("/3")
+def endpoint3():
+    print("Hello")
+    time.sleep(5)
+    print("Bye")
+    
+
+"""
+BEST PRACTICES
+1. Use async def for endpoint with non blocking I/O operations.
+2. Dont use async def for the endpoint blocking I/O operations.
+3. Use normal function for endpoints with blocking I/O operations
+"""
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PORT=${1:-8000}  # default to 8000 if no argument
 
 # Activate the virtual environment
 source venv/bin/activate
@@ -7,4 +8,4 @@ source venv/bin/activate
 pip install -r requirement.txt
 
 # Run the application with uvicorn
-uvicorn main:app --reload
+uvicorn main:app --reload --port $PORT
